aee053cd464b7d6070978f23b5db83c5bb73e8af
Thuytien4325 <thuyytien4325@gmail.com>
2024-11-11 05:36:30 +0700

    Add files via upload

---
+++ BusinessObject/TransactionDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaypalDemo.Models;
+
+namespace PaypalDemo
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext()
+        {
+        }
+
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TransactionPayPal>()
+                .HasKey(t => t.PaymentId);
+        }
+
+        public DbSet<TransactionPayPal> TransactionPayPals { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //LAPTOP-5CRSBD7F\\CMSSQLSERVER;Database=PayPalDemo;User Id=as;Password=123456;TrustServerCertificate=True
+            optionsBuilder.UseSqlServer("Server=.;Database=PayPalDemo;User Id=sa;Password=1234567890;TrustServerCertificate=True");
+        }
+    }
+}

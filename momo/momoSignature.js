a0d64ae85f13c8448aab00bc3b1031387df6754d
Thuytien4325 <thuyytien4325@gmail.com>
2024-11-18 06:00:59 +0700

    Delete momo directory

--- momo/momoSignature.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-const { accessKey, secretKey } = require('./momoConfig');
-
-function generateSignature(data) {
-    const rawSignature = `accessKey=${data.accessKey}&amount=${data.amount}&extraData=${data.extraData}&ipnUrl=${data.ipnUrl}&orderId=${data.orderId}&orderInfo=${data.orderInfo}&partnerCode=${data.partnerCode}&redirectUrl=${data.redirectUrl}&requestId=${data.requestId}&requestType=${data.requestType}`;
-    return crypto.createHmac('sha256', secretKey).update(rawSignature).digest('hex');
-}
-
-module.exports = generateSignature;
